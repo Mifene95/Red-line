c23553e8fcb98c9a29f2733c1adccb7f8ab51f3a
unknown <mifene95@gmail.com>
2024-06-05 15:04:37 +0100

    Adjusted logo and text

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,13 +1,16 @@
 export default function Header(){
   return (
       <>  
-          <div className="flex w-full h-[750px] bg-leather-texture justify-center">
-              <img
+          <div className="flex flex-col w-full h-[750px] bg-leather-texture justify-center items-center">
+            <div className="flex flex-col items-center"> <img
                   src="/images/redline2.png"
                   alt="red-line logo"
-                  className="h-[250px] w-[250px] mt-[200px]"
+                  className="h-[280px] w-[280px] mt-[120px]"
               />
-          </div>
+                <h1 className="text-white text-4xl mt-2">Calidad y profesionalidad</h1>
+                <h3 className="text-white text-2xl mt-5">Libera el potencial de tu vehiculo</h3></div>
+              </div>
+
       </>  
   )
 }
