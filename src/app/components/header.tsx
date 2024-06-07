febca81540418953be516bbd0ce9630c9936300f
Michael <mifene95@gmail.com>
2024-06-07 09:54:52 +0100

    Adjusted main logo

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,17 +1,22 @@
-export default function Header(){
+export default function Header() {
   return (
-      <>  
-          <div className="flex flex-col w-full h-[750px] bg-leather-texture justify-center items-center">
-            <div className="flex flex-col items-center"> <img
-                  src="/images/redline2.png"
-                  alt="red-line logo"
-                  className="h-[280px] w-[280px] mt-[120px]"
-              />
-                <h1 className="text-white text-4xl mt-2">Calidad y profesionalidad</h1>
-                <h3 className="text-white text-2xl mt-5">Libera el potencial de tu vehiculo</h3></div>
-              </div>
-
-      </>  
-  )
+    <>
+      <div className="flex flex-col w-full h-[940px] bg-leather-texture justify-center items-center">
+        <div className="flex flex-col items-center">
+          {" "}
+          <img
+            src="/images/redline2.png"
+            alt="red-line logo"
+            className="h-[350px] w-[350px] mt-[-30px]"
+          />
+          <h1 className="text-white text-4xl mt-4">
+            Calidad y profesionalidad
+          </h1>
+          <h3 className="text-white text-2xl mt-5">
+            Libera el potencial de tu vehiculo
+          </h3>
+        </div>
+      </div>
+    </>
+  );
 }
-
